b12dd270d3077016147ed9f60550d9555c4b4c74
Boris D <boris.dob@gmail.com>
2022-09-26 10:11:08 -0700

    Initial commit.

---
+++ lib/libwrap.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <caml/mlvalues.h>
+#include <caml/callback.h>
+#include <caml/alloc.h>
+
+#include "libwrap.h"
+
+void
+ini_lib (char** argv)
+{
+	caml_startup(argv);
+}
+
+int
+fib (int n)
+{
+	static const value * closure = NULL;
+	if (closure == NULL) closure = caml_named_value("fib");
+	return Int_val(caml_callback(*closure, Val_int(n)));
+}
+
+char *
+format_result (int n)
+{
+	static const value * closure = NULL;
+	if (closure == NULL) closure = caml_named_value("fib");
+	/* copy the C string to the C heap so that it remains valid after GC */
+	return strdup(String_val(caml_callback(*closure, Val_int(n))));
+}
---
+++ lib/libwrap.h
@@ -0,0 +1,3 @@
+void init_lib (char** argv);
+int fib (int);
+char * format_result (int);
